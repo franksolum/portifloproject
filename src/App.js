62d37428e26ba9dabc443d954a90203a5e08e335
franksolum <franksolum@gmail.com>
2021-08-04 05:28:16 +0000

    header completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+//import Particles from "react-particles-js";
+import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 function App() {
   return (
+    <>
+     {/** <Particles
+       params={{
+         particles: {
+           number:{
+             value:30,
+             density:{
+               enable: true,
+               value_area:900
+             }
+           }
+           
+         }
+       }}
+      
+      
+      />**/}
       <Navbar />
+      <Header />
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,32 +6,32 @@ import { faBars } from "@fortawesome/free-solid-svg-icons"
 
 export const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-  <div class="container">
-    <a class="navbar-brand" href="#"><img className="logo" src={logo} alt="logo...."/></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+  <div className="container">
+    <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo...."/></a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 
       <FontAwesomeIcon icon={faBars} style={{color:"white"}}/>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About me</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About me</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Services</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Services</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">How work</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">How work</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Portiflo</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Portiflo</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contacts</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contacts</a>
         </li>
        </ul> 
     </div>
